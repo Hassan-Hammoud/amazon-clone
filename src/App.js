7b4a2e036268da22e659a547bdfe8c473f895290
Hassan-Hammoud <hassanhammoud2000@gmail.com>
2024-01-15 20:00:14 +0200

    finished Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Header />} />
+        <Route path="*" element={<h1>Page Not Found</h1>} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+import Logo from "../Img/amazon-logo.png";
+import { LuSearch } from "react-icons/lu";
+import { BsMinecartLoaded } from "react-icons/bs";
+
+const Header = () => {
+  return (
+    <header>
+      <Link to="/">
+        <img src={Logo} className="header-logo" alt="Amazon-logo" />
+      </Link>
+      <div className="header-search">
+        <input type="text" className="header-input" />
+        <LuSearch className="header-searchIcon" />
+      </div>
+      <div className="header-nav">
+        <Link to="/login">
+          <div className="header-option">
+            <div className="header-optionLineOne">Hello Guest</div>
+            <div className="header-optionLineTwo">Sign In</div>
+          </div>
+        </Link>
+
+        <Link to="/orders">
+          <div className="header-option">
+            <div className="header-optionLineOne">Returns</div>
+            <div className="header-optionLineTwo">& orders</div>
+          </div>
+        </Link>
+
+        <Link to="/orders">
+          <div className="header-option">
+            <div className="header-optionLineOne">Your</div>
+            <div className="header-optionLineTwo">Prime</div>
+          </div>
+        </Link>
+
+        <Link to="/checkout">
+          <div className="header-optionBasket">
+            <BsMinecartLoaded
+              style={{ fontSize: "35px" }}
+              className="cartIcon"
+            />
+            <div className="header-optionLineTwo header-basketCount">5</div>
+          </div>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
